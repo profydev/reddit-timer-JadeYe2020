7ad31f1f7f28be3dc1efcc1ed8c8d2cfe9fab396
Jade Ye <w0430394@nscc.ca>
2022-08-15 11:54:10 -0300

    Add router with routes / and /search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root'),
+);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/search" />
+    </Routes>
+  </BrowserRouter>,
+);
